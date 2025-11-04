55f512ed16d9593893551409a87453f645e8468e
atticlabsdesign <joe.atticlabs@gmail.com>
2025-11-03 20:16:23 -0500

    rudementary working version

--- dbgcli.h
+++ dbgcli.h
@@ -1,8 +1,17 @@
 #ifndef DBGCLI_H
 #define DBGCLI_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define DBGCLI_DEFAULT_HELP \
+           .keyword = {"help", "h"}, \
+            .help = "List all commands and their descirptions",\
+            .cmdFunc = NULL, 
+    
+#define DBG_CLI_MAX_ARGS 10
 
 typedef enum {
     DBGCLI_SUCCESS,
@@ -12,29 +21,31 @@ typedef enum {
     DBGCLI_TOO_MANY_ARGS,
 } dbgCliError_t;
 
-
-
 typedef struct {
-    char keyword[2][5]; //one shorthand and one longhand supported
-    char* param; 
-    char* help;
-    char* options;
-    size_t paramLeng;
-    dbgCliError_t (*cmdFunc)(char * dataIn); //dataIn should be a an array, starting with the length of the array of custom data per command function
+    // uint8_t params; 
+    const char* help;
+    // size_t paramLeng;
+    dbgCliError_t (*cmdFunc)(unsigned int argc, char *argv[]); //dataIn should be a an array, starting with the length of the array of custom data per command function
+    const char *keyword[2]; //one shorthand and one longhand supported
+    // char options[][2]; //need to think about handling options more in the future
+
 }dbgCliCommand_t;
 
 typedef struct {
-    char*   description;
+    const char*   description;
     // size_t  descLeng;
-    char*   help;
-    size_t  helpLeng;
-    dbgCliCommand_t* commands;
-    size_t  cmdNum;
+    const char*   help;
+    // size_t  helpLeng;
+    const dbgCliCommand_t *commands;
+    const size_t  cmdNum;
     // dbgCliError_t (*parse)(char *input, size_t length, dbgCliCommand_t* commands, size_t cmdNum);
-} submenu_t;
+} dbgCliMenu_t;
 
-typedef struct {} menu_t;
 
-void printDBG(const char *restrict, ...);
+// void printDBG(const char *restrict, ...);
+
+dbgCliError_t parseInput (char * command, const dbgCliMenu_t *menu);
+void dbgCliPresent(const dbgCliMenu_t *menu);
+void dbgCliHelp (const dbgCliMenu_t *menu);
 
 #endif
--- src/dbgcli.c
+++ src/dbgcli.c
@@ -2,17 +2,108 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void printDBG(const char *restrict inputStr, ...){
-    //thius is not done yet
+//TODO: replace printf with something more generic
 
-char * buffer;
+void dbgCliErrorHandler(dbgCliError_t error){
+    switch (error) {
+        case DBGCLI_SUCCESS:
+        break;
 
-va_list args;
-va_start(args,inputStr);
+        case DBGCLI_UNKNOWN_KEYWORD:
+        printf("Error: Unknown Command\n\r");
+        break;
 
-sprintf(buffer, inputStr, args); //have sprintf handle formatting
+        case DBGCLI_UNKNOWN_PARAMETER:
+        printf("Error: Unknown Argument\n\r");
+        break;
 
-va_end(args);
+        case DBGCLI_TOO_FEW_ARGS:
+        printf("Error: Missing Arguments\n\r");
+        break;
 
-return;
+        case DBGCLI_TOO_MANY_ARGS:
+        printf("Error: Too Many Arguments\n\r");
+        break;
+
+        default: 
+            printf("Unkown Error\n\r");
+        break;
+    }
+    return;
 }
+
+
+// void printDBG(const char *restrict inputStr, ...){
+//     //thius is not done yet
+
+// char * buffer;
+
+// va_list args;
+// va_start(args,inputStr);
+
+// sprintf(buffer, inputStr, args); //have sprintf handle formatting
+
+// va_end(args);
+
+// return;
+// }
+
+dbgCliError_t parseInput (char * command, const dbgCliMenu_t *menu){
+    dbgCliError_t error = DBGCLI_SUCCESS;
+    char* token = strtok(command, " ");
+    bool valid = false;
+    size_t argc = 1;
+    char *argv[DBG_CLI_MAX_ARGS];
+
+    if (token == NULL) {
+        error = DBGCLI_UNKNOWN_KEYWORD;
+    } else if (strcmp(token, "help")==0) {
+        dbgCliHelp(menu);
+    } else {
+        size_t i; 
+        for (i = 1; i<menu->cmdNum; i++) { //help must always be the 0th command
+            if (strcmp(token, menu->commands[i].keyword[1]) == 0 || strcmp(token, menu->commands[i].keyword[0]) == 0) {
+                valid = true;
+                break;
+            }
+        }
+
+        if (valid) {
+            argv[0] = token;
+            token = strtok(NULL, " ");
+
+            while (token != NULL){
+                argc ++;
+                argv[argc-1] = token;
+                token = strtok(NULL, " ");
+            }
+            error = menu->commands[i].cmdFunc(argc, argv);
+
+        } else {
+            error = DBGCLI_UNKNOWN_KEYWORD;
+        }
+    }
+
+    dbgCliErrorHandler(error);
+
+    return error;
+}
+
+void dbgCliPresent(const dbgCliMenu_t *menu){
+    // printf("%c^[[1J\n\r", 27);
+    printf("\f");
+    printf("%s \n\r", menu->description);
+    printf("Enter ""help"" for a list of commands\n\r");
+    printf("There are: %d commands\n\r", menu->cmdNum);
+
+}
+
+void dbgCliHelp(const dbgCliMenu_t *menu){
+        printf("%s\n\r", menu->help);
+    for (size_t i = 0; i<menu->cmdNum; i++) {
+        printf("%s, %s    %s\n\r", menu->commands[i].keyword[0],menu->commands[i].keyword[1],menu->commands[i].help); 
+    }
+
+    return;
+}
+
